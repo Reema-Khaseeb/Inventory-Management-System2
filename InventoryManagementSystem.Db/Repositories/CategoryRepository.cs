d300653b23e4a4d9bbc1b43ecf2de320aed1b554
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-04-06 23:11:03 +0300

    `CategoryRespository` CRUD operations with existence checking

---
+++ InventoryManagementSystem.Db/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using InventoryManagementSystem.Db.Models;
+using InventoryManagementSystem.Db.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Db.Repositories;
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly InventoryManagementSystemDbContext _dbContext;
+
+    public CategoryRepository(InventoryManagementSystemDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<Category> CreateCategoryAsync(Category category)
+    {
+        _dbContext.Categories.Add(category);
+        await _dbContext.SaveChangesAsync();
+
+        return category;
+    }
+
+    public async Task UpdateCategoryAsync(Category category)
+    {
+        _dbContext.Categories.Update(category);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteCategoryAsync(Category category)
+    {
+        _dbContext.Categories.Remove(category);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<Category> GetCategoryByIdAsync(int categoryId)
+    {
+        return await _dbContext.Categories.FindAsync(categoryId);
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesAsync()
+    {
+        return await _dbContext.Categories.ToListAsync();
+    }
+
+    public async Task<bool> IsCategoryExistAsync(int categoryId)
+    {
+        return await _dbContext.Categories.AnyAsync(c => c.Id == categoryId);
+    }
+}
---
+++ InventoryManagementSystem.Db/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagementSystem.Db.Models;
+
+namespace InventoryManagementSystem.Db.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(Category category);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetCategoryByIdAsync(int categoryId);
+        Task UpdateCategoryAsync(Category category);
+        Task<bool> IsCategoryExistAsync(int categoryId);
+    }
+}
