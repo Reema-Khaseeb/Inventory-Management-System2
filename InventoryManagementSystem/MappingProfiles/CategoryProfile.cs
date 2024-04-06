cd175ccc1d5bc73a04ea6f25063269615d89ac94
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-04-07 01:39:39 +0300

    Create `CategoryDto` and `CategoryProfile`

---
+++ InventoryManagementSystem/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using InventoryManagementSystem.Db.Models;
+using InventoryManagementSystem.Dtos;
+
+namespace InventoryManagementSystem.MappingProfiles;
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+}
