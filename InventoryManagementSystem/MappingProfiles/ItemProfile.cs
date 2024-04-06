1e7065f9b1afde135bc68663348920727ccb1788
Reema Khaseeb <reemafkhaseeb@gmail.com>
2024-04-06 20:04:27 +0300

    Create `ItemDto` and `ItemProfile` for mapping

---
+++ InventoryManagementSystem/MappingProfiles/ItemProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using InventoryManagementSystem.Db.Models;
+using InventoryManagementSystem.Dtos;
+
+namespace InventoryManagementSystem.MappingProfiles;
+public class ItemMappingProfile : Profile
+{
+    public ItemMappingProfile()
+    {
+        CreateMap<Item, ItemDto>().ReverseMap();
+    }
+}
